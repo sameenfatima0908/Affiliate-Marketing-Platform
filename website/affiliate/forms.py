1da247ffdc58b60d35fc34ab6a25afb6320566b5
Sameen Fatima <sameen72.fatima@gmail.com>
2023-11-20 03:25:29 +0500

    Refactor README.md

--- website/affiliate/forms.py
+++ website/affiliate/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
